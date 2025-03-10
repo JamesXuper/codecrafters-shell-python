690fb8b0f7f7a03bd307938e081cb9df3dace4de
JamesXuper <xujames17237606@gmail.com>
2025-03-10 11:01:45 +1100

    debugging

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    user_input = input("$ ")
+    user_input = input(" ")
     print(f"{user_input}: command not found \n")
 
 if __name__ == "__main__":
