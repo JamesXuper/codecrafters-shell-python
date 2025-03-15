4c8da6a5a015efa14559fc97fb71bb38d2b41307
JamesXuper <xujames17237606@gmail.com>
2025-03-16 09:36:48 +1100

    change for echo

--- app/main.py
+++ app/main.py
@@ -8,16 +8,45 @@ BUILTIN_COMMANDS = ["echo", "exit", "type", "pwd", "cd"]
 def input_exit(argv):
     exit(int(argv[0]))
 
-def input_echo(user_input, argv):
+def input_echo(argv):
     result = []
     i = 0
-
-    if user_input[5:].startswith("'"):
-        print('single quotes detected')
-        # while i < len(argv):
-        #     arg = argv[i]
-    else:
-        sys.stdout.write(" ".join(argv) + "\n")     
+    
+    while i < len(argv):
+        arg = argv[i]
+        
+        # Check if this argument starts with a single quote
+        if arg.startswith("'") and not arg.endswith("'"):
+            # Start of a quoted section
+            quoted_text = [arg[1:]]  # Remove the opening quote
+            
+            # Keep adding arguments until we find the closing quote
+            j = i + 1
+            while j < len(argv) and not argv[j].endswith("'"):
+                quoted_text.append(argv[j])
+                j += 1
+                
+            # If we found the closing quote
+            if j < len(argv):
+                quoted_text.append(argv[j][:-1])  # Remove the closing quote
+                result.append(" ".join(quoted_text))
+                i = j  # Skip to after the quoted section
+            else:
+                # No closing quote found, treat as normal
+                result.append(arg)
+        
+        # If the argument is entirely enclosed in single quotes
+        elif arg.startswith("'") and arg.endswith("'") and len(arg) > 1:
+            # Remove the quotes and add as is
+            result.append(arg[1:-1])
+        
+        else:
+            # Regular argument
+            result.append(arg)
+            
+        i += 1
+    
+    sys.stdout.write(" ".join(result) + "\n")
 
 def input_type(argv):
     if argv[0] in BUILTIN_COMMANDS:
@@ -50,7 +79,7 @@ def main():
         if cmd == "exit":
             input_exit(argv)
         elif cmd == "echo":
-            input_echo(user_input, argv)
+            input_echo(argv)
         elif cmd == "type": #checking whether we know the 'type' of the builtin function
             input_type(argv)
         elif cmd == "pwd":
